6ec4ae603de026e1d10dbb22c057053fe0cc5249
GreenIT-Neal <skgt5565@greenit.cc>
2024-07-22 11:46:44 +0900

    [ADD] : 하노이 탑

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
 import programmers.level_0.Babbling;
 import programmers.level_0.CurseNumberThree;
 import programmers.level_0.FindCompositeNumber;
+import programmers.level_2.HanoiTop;
 
 public class Main {
 
     public static void main(String[] args) {
-        FindCompositeNumber findCompositeNumber = new FindCompositeNumber();
-        findCompositeNumber.FindCompositeNumber();
+//        FindCompositeNumber findCompositeNumber = new FindCompositeNumber();
+//        findCompositeNumber.FindCompositeNumber();
+//
+//        Babbling babbling = new Babbling();
+//        babbling.babbling();
+//
+//        CurseNumberThree curseNumberThree = new CurseNumberThree();
+//        curseNumberThree.CurseNumberThree(15);
 
-        Babbling babbling = new Babbling();
-        babbling.babbling();
-
-        CurseNumberThree curseNumberThree = new CurseNumberThree();
-        curseNumberThree.CurseNumberThree(15);
+        HanoiTop hanoiTop = new HanoiTop();
+        hanoiTop.hanoiTop(3, 'a', 'b', 'c');
     }
 }
